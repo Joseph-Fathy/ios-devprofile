46de84f0fce2f0b42c0db87d1f34fb993b147165
jo <joseph.gayed@vodafone.com>
2019-06-22 20:13:24 +0200

    empty project

---
+++ DevProfile/DevProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by Joe on 6/22/19.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
